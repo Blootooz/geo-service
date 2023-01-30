6d37bfce9e2a8db21e19e69a5e365519a67f44e9
Alexandra <84934027+Blootooz@users.noreply.github.com>
2023-01-30 20:18:15 +0300

    add test

---
+++ src/test/java/ru/netology/GeoServiceImplTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.testng.Assert.assertThrows;
+
+public class GeoServiceImplTest {
+
+    @ParameterizedTest(name = "Test for {0}")
+    @CsvSource(value = {"127.0.0.1, null", "172.0.32.11, Moscow", "96.44.183.149, New York", "172., Moscow", "96., New York"})
+    protected void testByIp(String ip, String expected) {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Location location = geoService.byIp(ip);
+
+        if (expected.equals("null")) {
+            expected = null;
+        }
+        assertEquals(expected, location.getCity());
+
+    }
+
+    @Test
+    protected void testByCoordinates() throws RuntimeException {
+        GeoServiceImpl geoService = new GeoServiceImpl();
+        Assert.assertThrows(RuntimeException.class, () -> geoService.byCoordinates(0, 0));
+    }
+
+}
